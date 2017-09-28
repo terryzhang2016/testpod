4cf0d50bc2db6ac5f9cb26c24a8750cbc7a29f5e
terry <zhangc@shinetechchina.com>
2017-09-28 14:32:08 +0800

    update code

---
+++ TestLib/Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  TestLib
+//
+//  Created by AirS CC on 28/09/2017.
+//  Copyright © 2017 AirS CC. All rights reserved.
+//
+
+import Foundation
+import SwiftString3
+
+public class TestClass {
+  public func show(_ text: String) {
+    print("This text is: \(text)")
+    print("This text is: \(String.count(text))")
+  }
+}
